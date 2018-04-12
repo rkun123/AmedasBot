5588650ed9b0191778f138920bb871671182706e
rkun123 <tren111213@gmail.com>
2018-04-13 01:32:42 +0900

    First Release

--- app.py
+++ app.py
@@ -1,22 +1,31 @@
 from twitter import *
 from tokensLoader import Loader
-from notifyScraper import Notify
+from scraper import Scraper
+import datetime
+
+TEMPLATE = """[飯塚アメダスBot]
+時刻: {0}時
+気温: {1}℃
+降水量:{2}mm
+風向: {3}
+風速: {4}m/s
+日照時間:{5}h
+湿度: {6}%
+気圧: {7}hPa
+http://www.jma.go.jp/jp/amedas_h/today-82136.html?areaCode=000&groupCode=58
+({8}投稿)"""
 
 tokensLoader = Loader()
 tokens = tokensLoader.load()
 auth=OAuth(tokens["TOKEN"],tokens["TOKEN_SECRET"],tokens["CONSUMER"],tokens["CONSUMER_SECRET"])
 t = Twitter(auth=auth)
 
-def notifycheck():
-    notifyGetter = Notify()
-    latestNotify = notifyGetter.getLatestDetail()
-    msgTemplate = """タイトル {0}
-日時 {1}
-時限等 {2}
-対象 {3}
-対象学年 {4}
-{5}"""
-    msg = msgTemplate.format(latestNotify["title"],latestNotify["date"],latestNotify["time"],latestNotify["target"],latestNotify["targetGrade"],latestNotify["url"])
+
+def tweet():
+    timeString = datetime.datetime.today().strftime("%H:%M")
+    sc = Scraper()
+    data = sc.scrape()
+    msg = TEMPLATE.format(data["time"],data["temp"],data["rain"],data["windAngle"],data["windSpeed"],data["sunrisetime"],data["humidity"],data["airplessure"],timeString)
     print(msg)
-    t.statuses.update(status=msg)
-notifycheck()
+    #t.statuses.update(status=msg)
+tweet()
---
+++ scraper.py
@@ -0,0 +1,35 @@
+from bs4 import BeautifulSoup
+from urllib import request
+
+class Scraper:
+    def __init__(self):
+        self.URL = "http://www.jma.go.jp/jp/amedas_h/today-82136.html?areaCode=000&groupCode=58"
+    
+    def scrape(self):
+        htmlBody = request.urlopen(self.URL)
+        soup = BeautifulSoup(htmlBody,"html.parser")
+        datalist = soup.find(id="tbl_list").find_all("tr")
+        datalist.pop(0)
+
+        for i in (range(23,0,-1)):
+            data = datalist[i].find_all("td",class_="block middle")
+            if  data[0].get_text() != "\xa0":
+                idx = datalist[i].find_all("td",class_="time left")
+                res = {}
+                res["time"] = idx[0].get_text()
+                res["temp"] = data[0].get_text()
+                res["rain"] = data[1].get_text()
+                res["windAngle"] = data[2].get_text()
+                res["windSpeed"] = data[3].get_text()
+                res["sunrisetime"] = data[4].get_text()
+                res["humidity"] = data[5].get_text()
+                res["airplessure"] = data[6].get_text()
+                print(res)
+                return res
+                
+
+
+
+#for debug
+sc = Scraper()
+sc.scrape()
